0c15398cfe260c9d123ad7ce724d4e657bfe0771
Jason Pescione <j@Jasons-MacBook-Air.local>
2019-05-21 23:54:40 -0400

    writing matrix function

--- perf/main.perf.js
+++ perf/main.perf.js
@@ -1,8 +1,48 @@
 const lib = require('../lib');
 const $ = lib;
 
-// console.log('no perf tests.');
+const mergeSort = require('../lib/ms');
+var data = [];
+var i = 1000000;
+while (i--) {
+  data.push(Math.round(Math.random() * 10));
+}
 
-var startTime = Date.now();
-$(1, 1000).forEach(i => console.log);
-var endTime = Date.now();
+function isSorted(original, sorted) {
+  if (original.length !== sorted.length) return false;
+  var last = sorted.shift();
+  return sorted.every(item => {
+    var result = item >= last;
+    last = item;
+    return result;
+  });
+};
+
+function test(obj, callback) {
+  var startTime = Date.now();
+  var result = callback.call(null, obj);
+  var endTime = Date.now();
+  if (!isSorted(data, result)) {
+    throw new Error('Not properly sorted');
+  }
+  return endTime - startTime;
+}
+
+var time;
+time = test(data, mergeSort);
+console.log('My merge sort: ', time);
+// time = test(data, sort);
+// console.log('Best not my sort: ', time);
+time = test(data, (data1) => {
+  return data1.sort(function(a, b) {
+    return a - b;
+  });
+});
+console.log('Native sort: ', time);
+
+
+
+///
+// var startTime = Date.now();
+// $(1, 1000).forEach(i => console.log);
+// var endTime = Date.now();
